7c6851c52ea240814e4d6863f310b3b9b0f798aa
Caleb <oyscaleb@gmail.com>
2023-02-03 23:44:36 +0800

    w4 commit3

---
+++ Week 4/Q4/src/ticketing/entity/AdvancedBookingTicket.java
@@ -0,0 +1,17 @@
+package ticketing.entity;
+
+public class AdvancedBookingTicket extends Ticket {
+    private int numDaysInAdvance;
+
+    public AdvancedBookingTicket(String title, int numDaysInAdvance) {
+        super(title);
+        this.numDaysInAdvance = numDaysInAdvance;
+    }
+
+    public double calculatePrice() {
+        double originalPrice = super.calculatePrice();
+        double discounted = originalPrice - 0.5 * numDaysInAdvance;
+
+        return Math.max(0.5 * originalPrice, discounted);
+    }
+}
---
+++ Week 4/Q4/src/ticketing/utility/TicketUtility.java
@@ -0,0 +1,20 @@
+package ticketing.utility;
+
+import java.util.*;
+
+import ticketing.entity.*;
+
+public class TicketUtility {
+    public static double calculateAverageTicketPrice(ArrayList<Ticket> ticketList) {
+        if (ticketList == null || ticketList.isEmpty()) {
+            return 0;
+        }
+
+        double sum = 0;
+        for (Ticket t : ticketList) {
+            sum += t.calculatePrice();
+        }
+
+        return sum / ticketList.size();
+    }
+}
